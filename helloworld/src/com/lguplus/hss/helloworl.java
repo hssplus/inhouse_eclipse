902cf99177f2999b1f19451ab9aa7392b9aa2839
User <User@192.168.219.120>
2022-04-14 14:28:01 +0900

    second commit

--- helloworld/src/com/lguplus/hss/helloworl.java
+++ helloworld/src/com/lguplus/hss/helloworl.java
@@ -5,6 +5,7 @@ public class helloworl {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
+		System.out.println("My name is HSS");
 		
 
 		// TODO Auto-generated method stub
